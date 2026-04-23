9d2e999cdc3cceaffc3b57c36ca99634200e09fa
Matt Schultz <mattschultz11@gmail.com>
2026-04-22 20:47:37 -0400

    Add team rating totals

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Card, CardBody, CardHeader } from "@heroui/react";
+import { Card, CardBody, CardHeader, Chip } from "@heroui/react";
 import Link from "next/link";
 
 import type { Position } from "@/graphql/generated";
+import { positionRating } from "@/utils/stringUtils";
 
 import TeamLogo from "./TeamLogo";
 import TeamName from "./TeamName";
@@ -55,14 +56,19 @@ export default function TeamCard({ team, league, season, hideNumber }: Props) {
   return (
     <Card>
       <CardHeader>
-        <div className="flex items-center gap-3">
-          <TeamLogo team={team} width={36} height={36} isBlurred />
-          <TeamName
-            as={Link}
-            team={team}
-            href={scheduleHref}
-            className="text-3xl font-semibold hover:underline focus-visible:underline"
-          />
+        <div className="flex w-full items-center justify-between gap-2">
+          <div className="flex items-center gap-3">
+            <TeamLogo team={team} width={36} height={36} isBlurred />
+            <TeamName
+              as={Link}
+              team={team}
+              href={scheduleHref}
+              className="text-3xl font-semibold hover:underline focus-visible:underline"
+            />
+          </div>
+          <Chip size="sm" variant="bordered">
+            {team.players.reduce((acc, player) => acc + (positionRating(player) ?? 0), 0)}
+          </Chip>
         </div>
       </CardHeader>
       <CardBody>
